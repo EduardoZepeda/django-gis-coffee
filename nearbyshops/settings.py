6a0d4b4d01279352566fde50a4208f1057d40c27
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-08 22:09:13 -0500

    Add gis engine to database configuration

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
