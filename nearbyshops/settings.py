96e57caf77c8f66e2b3b10a90d790802b35376ed
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-09 19:53:03 -0500

    Try to fix missing marker with FORCE_IMAGE_PATH in settings

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -120,11 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static_root/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
+
+# STATIC_ROOT = 'static_root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -148,6 +150,7 @@ if DEBUG:
     }
 
 LEAFLET_CONFIG = {
+    'FORCE_IMAGE_PATH': True,
     'DEFAULT_CENTER': (20.659698, -103.349609),
     'DEFAULT_ZOOM': 12,
 }
