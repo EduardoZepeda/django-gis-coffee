55d892572a0f49d94b0a994800e9209848d10a19
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-11 22:14:39 -0500

    Add CoffeeBag to shop detail and CSS

--- shops/admin.py
+++ shops/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 
-from .models import Shop
+from .models import Shop, CoffeeBag
 
 
 @admin.register(Shop)
 class ShopAdmin(OSMGeoAdmin):
     list_display = ("name", "location")
+
+admin.site.register(CoffeeBag)
