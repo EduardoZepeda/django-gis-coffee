843f56a2a62e4e3af9be982bc3e1be2d0188f268
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-14 14:47:24 -0500

    Add address to map and customize admin

--- shops/admin.py
+++ shops/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.admin import GISModelAdmin
 
 from .models import Shop, CoffeeBag
 
 
 @admin.register(Shop)
-class ShopAdmin(OSMGeoAdmin):
+class ShopAdmin(GISModelAdmin):
     list_display = ("name", "location")
+    search_fields = ("name", "address")
 
-admin.site.register(CoffeeBag)
+class CoffeeBagAdmin(admin.ModelAdmin):
+    search_fields = ("brand", "origin")
+    list_filter = ("origin", )
+
+admin.site.register(CoffeeBag, CoffeeBagAdmin)
+
+
+admin.site.site_header = 'Tamper'
+admin.site.index_title = 'Tamper Panel admin'
+admin.site.site_title = 'Tamper'
--- shops/views.py
+++ shops/views.py
@@ -43,7 +43,7 @@ class NearbyShops(generic.View):
         # geojson deals with point fields
         data = serialize('geojson', nearby_shops,
           geometry_field='location',
-          fields=('name', 'pk'))
+          fields=('name', 'pk', 'address'))
         return HttpResponse(data, content_type="application/json")
 
 class ShopDetail(generic.DetailView):
