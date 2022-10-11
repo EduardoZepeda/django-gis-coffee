344d42b9cd96805982fe4809ec41ebb22c082cd0
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-10-11 12:16:49 -0500

    Create custom user model and basic account views

---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from django.db import models
+
+class Contact(models.Model):
+    """A model for tracking user followers and following relationships"""
+    user_from = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='rel_to_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+
+class User(AbstractUser):
+    following = models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False)
+
+    def get_absolute_url(self):
+        return reverse("accounts:user_profile")
