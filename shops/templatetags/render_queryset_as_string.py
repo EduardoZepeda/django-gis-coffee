455382d00d846d72c1c461dea9cc88a9f4800176
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-10-16 16:52:58 -0500

    Add title and seo content for public pages

---
+++ shops/templatetags/render_queryset_as_string.py
@@ -0,0 +1,10 @@
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def render_queryset_as_string(queryset, attr=None):
+    query_list = []
+    for param in queryset:
+        query_list.append(getattr(param, attr) if attr else str(param))
+    return ', '.join(query_list)
