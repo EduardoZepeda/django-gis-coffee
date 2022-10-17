a402b8eb96b95de7edbbd47b867e0a03992840c7
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-10-17 11:03:55 -0500

    Add correct password handling for custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
 from .models import Contact, User
 
+user = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
 admin.site.register(Contact)
-admin.site.register(User)
+admin.site.register(user, CustomUserAdmin)
+
+
+
+
+
