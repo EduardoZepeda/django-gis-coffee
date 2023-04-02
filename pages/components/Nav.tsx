af8340dfa74279f156a2e0ccc8d30132f6815967
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 17:33:09 -0600

    add link to home on logo

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -3,6 +3,7 @@ import styles from '@styles/nav.module.css'
 import Menu from '@components/Menu'
 import Sidebar from '@components/Sidebar'
 import Burguer from '@components/Burguer'
+import Link from 'next/link'
 
 const Nav = () => {
     const [menuOpen, setMenuOpen] = useState<boolean>(false)
@@ -13,7 +14,7 @@ const Nav = () => {
 
     return (
         <nav className={styles.nav}>
-            <div className={styles.logo}>Tamper</div>
+            <div className={styles.logo}><Link href="/">Tamper</Link></div>
             <div className={styles.menu}>
                 <Menu />
             </div>
