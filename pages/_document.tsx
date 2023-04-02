de935b2c9b7f88548efc6592093f19aca6e1098e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 17:30:20 -0600

    move unpkg cdn to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap" rel="stylesheet" />
+        {/* Block leaflet requisites CDN */}
+        {/* Using version 1.7.1 because later versions have a missing marker icon bug
+        also make sure to not import leaflet.css otherwise this fix won't work */}
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin="" />
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+          integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+          crossOrigin=""></script>
+        {/* EndBlock leaflet requisites CDN */}
       </Head>
       <body>
         <Main />
