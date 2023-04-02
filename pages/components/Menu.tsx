ca74e00c8d07cbe88d72eeadff34073159dd7b52
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 17:33:45 -0600

    Add some example links to menu

--- pages/components/Menu.tsx
+++ pages/components/Menu.tsx
@@ -5,12 +5,11 @@ import Link from 'next/link'
 const Menu = () => {
     return (
         <ul className={styles.list}>
-            <li className={styles.item}><Link href="">link</Link></li>
-            <li className={styles.item}><Link href="">link</Link></li>
-            <li className={styles.item}><Link href="">link</Link></li>
-            <li className={styles.item}><Link href="">link</Link></li>
-            <li className={styles.item}><Link href="">link</Link></li>
-            <li className={styles.item}><Link href="">link</Link></li>
+            <li className={styles.item}><Link href="/about">About</Link></li>
+            <li className={styles.item}><Link href="/register">Register</Link></li>
+            <li className={styles.item}><Link href="/login">Login</Link></li>
+            <li className={styles.item}><Link href="/logout">Logout</Link></li>
+            <li className={styles.item}><Link href="/new-additions">New additions</Link></li>
         </ul>
     )
 }
