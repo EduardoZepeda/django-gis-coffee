42a82f274187511f60d6d09b81eb767c3deec16e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 17:34:10 -0600

    Remove installed leaflet css, prevent missing icon

--- pages/components/Map.tsx
+++ pages/components/Map.tsx
@@ -1,18 +1,18 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { GdlLocation } from '@components/GuadalajaraLocation';
+import { LocationMarker } from '@components/LocationMarker'
+
 
 const Map = () => {
+
     return (
-        <MapContainer center={[20.659698, -103.349609]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+        <MapContainer center={GdlLocation} zoom={12} scrollWheelZoom={true} style={{ height: "100%", width: "100%" }}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[20.659698, -103.349609]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            <LocationMarker />
         </MapContainer>
     )
 }
