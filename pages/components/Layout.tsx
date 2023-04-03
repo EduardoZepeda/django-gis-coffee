76f45e5f8277369d51243f726808e1f088b2ef97
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 22:44:47 -0600

    Fix typo in css import

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '@styles/layout.module.css'
 import Nav from '@components/Nav'
 import Footer from '@components/Footer'
-import mainStyles from '@/styles/Home.module.css'
+import mainStyles from '@styles/Home.module.css'
 
 interface propsWithChildren {
     children: React.ReactNode
