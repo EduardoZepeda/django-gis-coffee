059a097b24cf53643565ab5179848d3cc595f773
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-02 22:45:24 -0600

    Add api rotues to index file

--- pages/urls/index.tsx
+++ pages/urls/index.tsx
@@ -1,2 +1,3 @@
-export const shopsByLocation = `http://localhost:8000/api/v1/shops/`
-
+export const shopsByLocation = new URL('http://127.0.0.1:8000/api/v1/shops/')
+export const loginUrl = new URL('http://127.0.0.1:8000/api/v1/authentication/login/')
+export const getCurrentUser = new URL('http://127.0.0.1:8000/api/v1/authentication/user/')
