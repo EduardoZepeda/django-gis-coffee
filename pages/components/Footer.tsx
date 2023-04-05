2afec27364f8794a26741192a013cf4abdf020df
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-05 00:19:24 -0600

    Add static pages and social media icons

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import styles from '@styles/footer.module.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
 
 const Footer = () => {
+    const size = 'xl'
+
     return (
         <footer className={styles.footer}>
-            footer
+            <div className={styles.menu}>
+                <div className={styles.column}>
+                    <h3>Coffee methods</h3>
+                    <Link href="/chemex">Chemex</Link>
+                    <Link href="/v60">V60</Link>
+                    <Link href="/french-press">French press</Link>
+                </div>
+            </div>
+            <div className={styles.social}>
+                {/* <FontAwesomeIcon size={size} icon={faFacebook} />
+                <FontAwesomeIcon size={size} icon={faInstagram} />
+                <FontAwesomeIcon size={size} icon={faTiktok} /> */}
+            </div>
+            <div className={styles.legal}>
+                <Link href="/cookies">Cookies Policy</Link>
+                <Link href="/terms">Terms of use Policy</Link>
+            </div>
         </footer>
     )
 }
