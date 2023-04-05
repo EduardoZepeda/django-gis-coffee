37560974a7c2b608195a1fa01a76d06cd9156ac0
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-05 10:25:46 -0600

    Add coffee card component

---
+++ components/CoffeeCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '@styles/coffeeCard.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const CoffeeCard = ({ id, properties: { name, address } }: FeaturesEntity) => {
+    return (
+        <div className={styles.card}>
+            <Link href={`coffee-shops/${id}`}>
+                <div className={styles.a}>
+                    <Image className={styles.image} src={'/cafeteria_1.jpg'} height={300} width={300} alt={'Coffee shop photography'} />
+                    <div><strong>{name}</strong></div>
+                    <div className={styles.address}><small>{address}</small></div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default CoffeeCard
