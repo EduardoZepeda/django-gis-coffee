786be969d627d149ff83a7af62dcf802222ae686
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-05 00:20:42 -0600

    Update api urls

--- pages/urls/index.tsx
+++ pages/urls/index.tsx
@@ -1,3 +1,8 @@
 export const shopsByLocation = new URL('http://127.0.0.1:8000/api/v1/shops/')
 export const loginUrl = new URL('http://127.0.0.1:8000/api/v1/authentication/login/')
 export const getCurrentUser = new URL('http://127.0.0.1:8000/api/v1/authentication/user/')
+export const registerUserUrl = new URL('http://127.0.0.1:8000/api/v1/registration/')
+export const resetPasswordUrl = new URL('http://127.0.0.1:8000/api/v1/authentication/password/reset/')
+export const getNewestCoffeeShopsUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
+export const getCoffeeShopByIdUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
+export const likeCoffeeShopUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
