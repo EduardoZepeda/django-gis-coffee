fa52aadf14b500b2118c143b283fe3993a6a0e5a
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-05 10:28:32 -0600

    Move dynamic import outside of map component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
+// Prevents window not defined error 
+// See more here https://stackoverflow.com/questions/57704196/leaflet-with-next-js
+const Map = dynamic(
+  () => import('../components/Map'), // replace '@components/map' with your component's location
+  { ssr: false } // This line is important. It's what prevents server-side render
+)
+
 export default function Home() {
-  // Prevents window not defined error 
-  // See more here https://stackoverflow.com/questions/57704196/leaflet-with-next-js
-  const Map = dynamic(
-    () => import('@components/Map'), // replace '@components/map' with your component's location
-    { ssr: false } // This line is important. It's what prevents server-side render
-  )
+
   return (
     <>
       <Head>
