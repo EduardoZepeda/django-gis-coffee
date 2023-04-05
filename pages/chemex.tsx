b87a5602905a0e7f8656985f895108fbddb82288
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-05 00:08:20 -0600

    Add static pages to layout

---
+++ pages/chemex.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from '@styles/pages.module.css'
+import Head from 'next/head'
+
+const Chemex = () => {
+    return (
+        <div className={styles.content}>
+            <Head>
+                <title>Tamper | Chemex</title>
+                <meta name="description" content="Chemex brewing method introduction" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h2>The chemex brewing method</h2>
+            <p>If you're a coffee lover looking for a new and unique way to prepare your morning brew, then you might want to give the Chemex coffee method a try. The Chemex is a pour-over coffee maker that has been around since the 1940s, and it has gained a loyal following among coffee enthusiasts for its ability to produce a clean, smooth, and flavorful cup of coffee.</p>
+            <p>The Chemex coffee method involves placing a special paper filter in the top of the Chemex carafe and then adding freshly ground coffee to the filter. You then pour hot water over the coffee grounds in a slow, circular motion, allowing the water to saturate the grounds and extract the flavor from the beans.</p>
+            <p>One of the benefits of the Chemex coffee method is that it allows you to control the brewing process, so you can adjust the flavor and strength of your coffee to suit your tastes. Plus, because the Chemex uses a paper filter, it produces a cleaner cup of coffee than other methods that use metal filters.</p>
+            <p>To get started with the Chemex coffee method, you'll need a few things, including a Chemex carafe, Chemex paper filters, a kettle for heating water, and freshly roasted coffee beans. You'll also need a coffee grinder to grind the beans just before brewing, as this will help to preserve the flavor and aroma of the coffee.</p>
+            <p>Once you have your supplies, you can start by heating the water to just below boiling point. While the water is heating, place a paper filter in the top of the Chemex carafe and add the freshly ground coffee. When the water is ready, pour a small amount over the coffee grounds to allow them to bloom, and then continue pouring the water in a slow, circular motion.</p>
+            <p>The amount of coffee and water you use will depend on your personal preference, but a good rule of thumb is to use one tablespoon of coffee for every six ounces of water. Experiment with different amounts of coffee and water to find the perfect balance for your taste.</p>
+            <p>Overall, the Chemex coffee method is a great way to enjoy a delicious and satisfying cup of coffee. With a little practice and experimentation, you can perfect your brewing technique and enjoy a truly unique and flavorful cup of coffee every morning.</p>
+        </div>
+    )
+}
+
+export default Chemex
---
+++ pages/terms.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from '@styles/pages.module.css'
+import Head from 'next/head'
+
+const Terms = () => {
+    return (
+        <div className={styles.content}>
+            <Head>
+                <title>Tamper | Terms of use</title>
+                <meta name="description" content="Terms of Use for Tamper" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h2>Terms of Use for Tamper</h2>
+            <p>Effective date: April 3, 2023</p>
+            <p>Welcome to Tamper, a website located at current url operated by Tamper Inc. (“Tamper,” “we,” or “us”). These Terms of Use (“Terms”) govern your use of the Site, so please read them carefully. By accessing or using the Site, you agree to be bound by these Terms, which constitute a legally binding agreement between you and Tamper.</p>
+            <h2>Your Use of the Site</h2>
+            <p>You may use the Site only for lawful purposes and in accordance with these Terms. You must not use the Site:</p>
+            <p>• In any way that violates any applicable federal, state, local, or international law or regulation.</p>
+            <p>• To engage in any conduct that restricts or inhibits anyone's use or enjoyment of the Site, or which, as determined by us, may harm Tamper or users of the Site or expose them to liability.</p>
+            <p>• To transmit, or procure the sending of, any advertising or promotional material, including any “junk mail,” “chain letter,” “spam,” or any other similar solicitation.</p>
+            <p>• To impersonate or attempt to impersonate Tamper, a Tamper employee, another user, or any other person or entity.</p>
+            <p>• To engage in any other conduct that restricts or inhibits anyone's use or enjoyment of the Site, or which, as determined by us, may harm Tamper or users of the Site or expose them to liability.</p>
+            <h2>User Accounts</h2>
+            <p>You may need to create an account to access certain features of the Site. When you create an account, you must provide accurate and complete information. You are solely responsible for maintaining the confidentiality of your account and password and for restricting access to your computer, and you agree to accept responsibility for all activities that occur under your account or password.</p>
+            <h2>Intellectual Property Rights</h2>
+            <p>The Site and its entire contents, features, and functionality (including but not limited to all information, software, text, displays, images, video, and audio, and the design, selection, and arrangement thereof), are owned by Tamper, its licensors, or other providers of such material and are protected by United States and international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.</p>
+            <h2>Disclaimers and Limitations of Liability</h2>
+            <p>The Site is provided on an “as is” and “as available” basis. Tamper makes no representations or warranties of any kind, express or implied, as to the operation of the Site or the information, content, materials, or products included on the Site.</p>
+            <p>In no event will Tamper be liable for any direct, indirect, special, punitive, incidental, or consequential damages arising out of or in connection with the use or inability to use the Site or the information, content, materials, or products included on the Site, including, without limitation, loss of revenue, loss of profits, or loss of data.</p>
+            <h2>Indemnification</h2>
+            <p>You agree to indemnify, defend, and hold harmless Tamper, its affiliates, licensors, and service providers, and its and their respective officers, directors, employees, contractors, agents, licensors, suppliers, successors, and assigns from and against any claims, liabilities, damages, judgments, awards, losses, costs, expenses, or fees (including reasonable attorneys' fees) arising out of or relating to your violation of these Terms or your use of the Site, including, but not limited to, your User Contributions, any use of the Site's content, services, and products other than as expressly authorized in these Terms, or your use of any information obtained from the Site.</p>
+            <h2>Termination</h2>
+            <p>Tamper may terminate or suspend your access to all or part of the Site</p>
+
+        </div>
+    )
+}
+
+export default Terms
