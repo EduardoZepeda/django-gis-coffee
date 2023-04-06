dd78a7415313c2011e7a5b136e99328795ec5a7f
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-06 17:40:13 -0600

    Add more types and generic succes, error type

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,12 @@ type resetPasswordType = {
     email: string,
 }
 
+type reviewSchemaType = {
+    recommend: boolean,
+    content: string,
+    shop: string
+}
+
 type NewUserType = {
     username: string
     email: string,
@@ -47,6 +53,7 @@ type Properties = {
     likes?: (LikesEntity | null)[] | null;
     content?: string | null;
     url: string;
+    liked: boolean;
 }
 type LikesEntity = {
     username: string;
@@ -54,7 +61,7 @@ type LikesEntity = {
     id: number;
 }
 
-type ErrorProp = {
+type MessageProp = {
     message: string
 }
 
@@ -65,14 +72,52 @@ type RenderStarsProps = {
 type LikesProps = {
     likes: Properties["likes"]
     id: FeaturesEntity["id"]
+    liked: Properties["liked"]
 }
 
 type LikeCoffeeShopType = FeaturesEntity["id"]
 
 type CustomMarkerProps = {
+    id: number
     coordinates: LatLngExpression
     name: Properties["name"]
     address: Properties["address"]
     roaster: Properties["roaster"]
     rating: Properties["rating"]
+}
+
+
+type Reviews = {
+    count: number;
+    next: string;
+    previous?: null;
+    results?: (ReviewResultsEntity)[] | null;
+}
+type ReviewResultsEntity = {
+    content: string;
+    recommended: boolean;
+    user: ReviewUser;
+    shop: string;
+    created_date: string;
+    modified_date: string;
+    url: string;
+}
+type ReviewUser = {
+    username: string;
+    url: string;
+    id: number;
+}
+
+type ReviewPostUpdate = {
+    content: string;
+    recommend: boolean;
+    shop: string
+}
+
+type ReviewFormProps = {
+    id: string
+}
+
+type SearchSchemaType = {
+    query: string
 }
