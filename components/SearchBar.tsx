6455abfd7cd6efe0e50b41b41bb9aa8c2fdfc0ac
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-06 17:41:28 -0600

    Add search bar component inside menu

---
+++ components/SearchBar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useForm, SubmitHandler } from "react-hook-form";
+import styles from '@styles/forms.module.css'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import Router from 'next/router'
+
+const SearchBar = () => {
+    const router = Router
+    const searchSchema = z.object({
+        query: z.string().min(3).max(255),
+    })
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<SearchSchemaType>(
+        {
+            resolver: zodResolver(searchSchema)
+        }
+    )
+
+    const onSubmit: SubmitHandler<SearchSchemaType> = data => {
+        router.push(`/search/${data.query}`)
+        reset()
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <input
+                id={"query"}
+                className={`${styles.input} ${errors.query ? styles.inputError : styles.inputValid}`}
+                defaultValue=""
+                {...register("query",
+                    {
+                        required: true,
+                    })}
+                placeholder="Search" />
+        </form>
+    )
+}
+
+export default SearchBar
---
+++ pages/search/[query].tsx
@@ -0,0 +1,52 @@
+import { useId } from 'react'
+import Head from 'next/head'
+import { useQuery } from 'react-query'
+import { getCoffeeShopQuery } from '@services/coffeeShops'
+import CoffeeCard from '@components/CoffeeCard'
+import styles from '@styles/newestAdditions.module.css'
+import Loader from '@components/Loader'
+import Error from '@components/Error'
+import { useRouter } from 'next/router'
+
+export default function NewestAdditions() {
+    const coffeeCardId = useId()
+    const router = useRouter()
+    const { query } = router.query
+    const { data, error, isLoading } = useQuery({
+        queryKey: [`search-${query}`],
+        queryFn: () => getCoffeeShopQuery(query)
+    })
+
+    if (error) {
+        return <Error message={"Error"} />
+    }
+    if (isLoading) {
+        return <Loader />
+    }
+
+    if (data) {
+        return (
+            <>
+                <Head>
+                    <title>Tamper | search: {query}</title>
+                    <meta name="description" content="Our newest additions to our database, let us find your favorite speciality coffee shop in Guadalajara" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <h2>Coffee Shops that contains: {query}</h2>
+                <div className={styles.container}>
+                    {data.count > 0 ? data.results.features.map(({ id, properties, type, geometry }: FeaturesEntity) =>
+                        <CoffeeCard
+                            id={id}
+                            properties={properties}
+                            type={type}
+                            geometry={geometry}
+                            key={`${coffeeCardId}-${id}`} />
+                    ) : <div>Sorry, we didn't found any coffee shop</div>
+                    }
+                </div>
+            </>
+        )
+    }
+
+}
