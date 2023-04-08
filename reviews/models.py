9dea54cbe2c9f914c7b032325c4e9e7c0f453a9f
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-07 23:51:59 -0600

    remove default delete from Review model

--- reviews/models.py
+++ reviews/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class Review(models.Model):
     content = models.CharField(max_length=255)
-    recommended = models.BooleanField(default=True)
+    recommended = models.BooleanField()
     user = models.ForeignKey(User, related_name="reviews", on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, related_name="reviews", on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
