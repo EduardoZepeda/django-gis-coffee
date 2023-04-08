09b236ad3c2450e7ff03b5ebc782a6419363d684
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-07 23:58:04 -0600

    set min height for main map

--- components/Map.tsx
+++ components/Map.tsx
@@ -7,13 +7,15 @@ import { LocationMarker } from '@components/LocationMarker'
 const Map = () => {
 
     return (
-        <MapContainer center={GdlLocation} zoom={12} scrollWheelZoom={true} style={{ height: "100%", width: "100%" }}>
+        <MapContainer center={GdlLocation} zoom={12} scrollWheelZoom={true} style={{
+            height: "100%", width: "100%", minHeight: '800px'
+        }}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <LocationMarker />
-        </MapContainer>
+        </MapContainer >
     )
 }
 
