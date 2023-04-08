cab0ba179ee36982fcdb708c48d13f9e2285b28f
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-07 23:56:57 -0600

    add link to coffee shop on marker on map

--- components/CustomMarker.tsx
+++ components/CustomMarker.tsx
@@ -1,15 +1,25 @@
 import React from "react"
-import { Marker, Popup, Tooltip } from 'react-leaflet'
+import { Marker, Tooltip } from 'react-leaflet'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFire } from '@fortawesome/free-solid-svg-icons'
 import RenderStars from "./RenderStars"
+import Router from "next/router"
 
-export const CustomMarker = ({ coordinates, name, address, roaster, rating }: CustomMarkerProps) => {
+export const CustomMarker = ({ coordinates, name, address, roaster, rating, id }: CustomMarkerProps) => {
+    const router = Router
+
 
     return (
-        <Marker position={coordinates}>
-            <Popup>{ }</Popup>
-            <Tooltip direction="bottom" offset={[0, 20]} opacity={0.8} permanent>
+        <Marker eventHandlers={{
+            click: (e) => {
+                router.push(`/coffee-shops/${id}`)
+            },
+        }} position={coordinates} >
+            <Tooltip eventHandlers={{
+                click: (e) => {
+                    router.push(`/coffee-shops/${id}`)
+                },
+            }} direction="bottom" offset={[0, 20]} opacity={0.8} permanent>
                 <div><strong>{name}</strong></div>
                 <small>{address}</small>
                 <div><RenderStars rating={rating} /></div>
