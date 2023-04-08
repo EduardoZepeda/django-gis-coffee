0c4aa0e9d59ee4efd562e31b93cc9423d732a23f
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-07 23:48:47 -0600

    Add  is request user or read only to users

--- utils/permissions/api_permissions.py
+++ utils/permissions/api_permissions.py
@@ -2,18 +2,22 @@ from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_MET
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class IsStaffOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS or request.user.is_staff
+
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
+
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
 
-class IsRequestUser(BasePermission):
+
+class IsRequestUserOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj
+        return request.user == obj or request.method in SAFE_METHODS
