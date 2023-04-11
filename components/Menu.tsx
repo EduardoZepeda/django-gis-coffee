5df77f7dc7cb24cf0336c2cfb747b735b1dbd185
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:12:35 -0600

    add feed to menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '@styles/menu.module.css'
 import Link from 'next/link'
 import { useSession, signOut } from "next-auth/react"
 import SearchBar from '@components/SearchBar'
+import Feed from '@components/Feed'
 
 const Menu = () => {
     const { data: session } = useSession()
+    const [openFeed, setOpenFeed] = useState<boolean>(false)
+
 
     return (
-        <ul className={styles.list}>
-            <li><SearchBar /></li>
-            <li className={styles.item}><Link href="/about">About</Link></li>
-            {session ? null : <li className={styles.item}><Link href="/auth/register">Register</Link></li>}
-            {session ? null : <li className={styles.item}><Link href="/auth/signin">Login</Link></li>}
-            {session ? <li className={styles.item}><Link href={`/users/${session.user?.username}`}>Profile</Link></li> : null}
-            {session ? <li onClick={() => signOut()} className={styles.item}>Logout</li> : null}
-            <li className={styles.item}><Link href="/new-additions">New additions</Link></li>
-        </ul>
+        <>
+            <ul className={styles.list}>
+                <li><SearchBar /></li>
+                <li className={styles.item}><Link href="/about">About</Link></li>
+                {session ? null : <li className={styles.item}><Link href="/auth/register">Register</Link></li>}
+                {session ? null : <li className={styles.item}><Link href="/auth/signin">Login</Link></li>}
+                {session ? <li className={styles.item}><Link href={`/users/${session.user?.username}`}>Profile</Link></li> : null}
+                {session ? <li onClick={() => signOut()} className={styles.item}>Logout</li> : null}
+                {session ? <li onClick={() => setOpenFeed(!openFeed)} className={styles.item}>Feed</li> : null}
+                <li className={styles.item}><Link href="/new-additions?page=1">New additions</Link></li>
+            </ul>
+            <Feed openFeed={openFeed} setOpenFeed={setOpenFeed} />
+        </>
     )
 }
 
