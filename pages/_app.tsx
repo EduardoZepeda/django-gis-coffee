4ff0ce6238b9735221d1a3300f66cc3f35933a2e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:14:25 -0600

    prevent font awesome icons to render big on start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app'
 import Layout from '@components/Layout'
 import { SessionProvider } from "next-auth/react"
 import { QueryClient, QueryClientProvider } from 'react-query'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 const queryClient = new QueryClient()
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
