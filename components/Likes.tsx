382086185bd0eb78ebb7a4540ea2a2f50267addd
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:12:21 -0600

    refactor mutation function in likes

--- components/Likes.tsx
+++ components/Likes.tsx
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import styles from '@styles/likes.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as faEmptyHeart } from '@fortawesome/free-regular-svg-icons'
-import { useSession } from "next-auth/react"
-import { useMutation } from 'react-query'
-import { LikeCoffeeShop, UnlikeCoffeeShop } from '@services/coffeeShops'
-import Link from 'next/link'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import styles from '@styles/likes.module.css';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faEmptyHeart } from '@fortawesome/free-regular-svg-icons';
+import { fetchDelete, fetchPost } from '@fetchUtils/useFetch';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { likeCreate, likeDestroy } from '@urls/index';
+import { useMutation } from 'react-query';
+import { useQueryClient } from 'react-query';
+import { useSession } from 'next-auth/react';
+
 
 const Likes = ({ liked, likes, id }: LikesProps) => {
-    const { data: session } = useSession()
+    const queryClient = useQueryClient()
+    const [userLikedShop, setUserLikedShop] = useState<boolean>(liked)
     const [likesNumber, setLikesNumber] = useState<number>(likes?.length || 0)
+    const { data: session } = useSession()
+    const token = session?.user?.token
     if (!session) {
         return (
             <div className={styles.likes}>
@@ -21,19 +27,20 @@ const Likes = ({ liked, likes, id }: LikesProps) => {
         )
     }
     // if session id is in likes' array, set userLikedShop to false, if not to true
-    const [userLikedShop, setUserLikedShop] = useState<boolean>(liked)
-    const mutation = useMutation(userLikedShop ? UnlikeCoffeeShop : LikeCoffeeShop, {
+    const { mutate, isLoading } = useMutation({
+        mutationFn: () => (userLikedShop ? fetchDelete(likeDestroy(id.toString()), token) : fetchPost(likeCreate(id.toString()), {}, token)),
         onSuccess: () => {
             setLikesNumber(userLikedShop ? likesNumber - 1 : likesNumber + 1)
             setUserLikedShop(!userLikedShop)
-        }
+            queryClient.invalidateQueries({ queryKey: ['coffeeShops'] })
+        },
     })
 
     useEffect(() => { }, [userLikedShop, likesNumber])
 
     return (
         <>
-            {likes ? <div onClick={() => mutation.mutate(id)} className={styles.likes}>
+            {likes ? <div onClick={() => { mutate() }} className={styles.likes}>
                 <FontAwesomeIcon size={'lg'} icon={userLikedShop ? faHeart : faEmptyHeart} />
                 {" "}<strong>{likesNumber}
                 </strong>
