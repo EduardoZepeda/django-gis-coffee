b76cf410bd226eb08fce985a43e6b3f06d562450
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:11:47 -0600

    Add user's feed component

---
+++ components/Feed.tsx
@@ -0,0 +1,46 @@
+import Error from '@components/Error';
+import FeedItem from '@components/FeedItem';
+import Loader from '@components/Loader';
+import React, { useEffect } from 'react';
+import styles from '@styles/feed.module.css';
+import { feedUrl } from '@urls/index';
+import { fetchGet } from '@fetchUtils/useFetch';
+import { useQuery } from 'react-query';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import Cross from '@components/Cross';
+
+const Feed = ({ openFeed, setOpenFeed }: FeedProps) => {
+    const { data: session, status } = useSession()
+    const token = session?.user?.token
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["coffeeShops", session?.user?.username],
+        queryFn: () => fetchGet(feedUrl, token),
+        enabled: status !== 'loading' && status !== 'unauthenticated'
+    })
+
+    if (status === 'unauthenticated') {
+        return null
+    }
+    return (
+        <div className={`${styles.container} ${openFeed ? '' : styles.hidden}`}>
+            <h3>Feed</h3>
+            <Cross show={openFeed} handleClick={setOpenFeed} />
+            <div className={styles.user}>
+                {error ? <Error message={"We couldn't get your feed. Please try refreshing the page."} /> : null}
+                {isLoading ? <Loader /> : null}
+                {data ? <ul>
+                    {data?.results?.map(({ action, target, user, created }: Feed) => <li>
+                        <FeedItem action={action} target={target} user={user} created={created} />
+                    </li>)
+                    }
+                </ul>
+                    :
+                    null}
+
+            </div>
+        </div>
+    )
+}
+
+export default Feed
