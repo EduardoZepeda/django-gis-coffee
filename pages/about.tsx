be0b6e727956813306a3051d701667f69329a943
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:14:37 -0600

    add custom head to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '@styles/pages.module.css'
+import Head from 'next/head'
 
 const About = () => {
     return (
         <>
+            <Head>
+                <title>Tamper | About</title>
+                <meta name="description" content="We want to offer you the best speciality coffee shops reviews in Guadalajara" />
+            </Head>
             <div className={styles.content}>
                 <h1>About me</h1>
                 <Image src={'/sui_400.jpg'} alt='Photography of the founder of Tamper, Sui' width={400} height={400} />
