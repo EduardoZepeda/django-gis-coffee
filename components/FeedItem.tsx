b4464510dd04ac4b689f63d8a21752ed632def5c
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:11:47 -0600

    Add user's feed component

---
+++ components/FeedItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '@styles/feed.module.css'
+import Link from 'next/link'
+
+const FeedItem = ({ user: { username: user, profile_picture }, action, target: { id, name, username }, created }: Feed) => {
+    return (
+        <div className={styles.feedItem}>
+            <div className={styles.profilePicture}>
+                <img src={profile_picture} className={styles.img} alt={`feed profile picture`} />
+            </div>
+            <div className={styles.action}>
+                <Link href={`/${action === 'followed' ? `users/${username}` : `coffee-shops/${id?.toString()}`}`}>
+                    {`${user} ${action} ${username ? username : name}`}
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default FeedItem
