e59437abe57aa0664778194f23543681755f48fd
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:11:13 -0600

    Update urls, add dynamic function urls

--- urls/index.tsx
+++ urls/index.tsx
@@ -1,12 +1,90 @@
-export const shopsByLocation = new URL('http://127.0.0.1:8000/api/v1/shops/')
-export const loginUrl = new URL('http://127.0.0.1:8000/api/v1/authentication/login/')
-export const getCurrentUser = new URL('http://127.0.0.1:8000/api/v1/authentication/user/')
-export const registerUserUrl = new URL('http://127.0.0.1:8000/api/v1/registration/')
-export const resetPasswordUrl = new URL('http://127.0.0.1:8000/api/v1/authentication/password/reset/')
-export const getNewestCoffeeShopsUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
-export const getCoffeeShopByIdUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
-export const likeCoffeeShopUrl = new URL('http://127.0.0.1:8000/api/v1/shops/')
-export const getCoffeeShopReviewsUrl = new URL('http://127.0.0.1:8000/api/v1/reviews/')
-export const getUserByUsernameUrl = new URL('http://127.0.0.1:8000/api/v1/users/')
-export const followUserUrl = new URL('http://127.0.0.1:8000/api/v1/users/')
-export const unfollowUserUrl = new URL('http://127.0.0.1:8000/api/v1/users/')
+const base = process.env.NEXT_PUBLIC_BACKEND_API
+
+// Authentication
+export const shopsByLocation = new URL(`${base}/api/v1/shops/`)
+export const loginUrl = new URL(`${base}/api/v1/authentication/login/`)
+export const getCurrentUserUrl = new URL(`${base}/api/v1/authentication/user/`)
+export const registerUserUrl = new URL(`${base}/api/v1/registration/`)
+export const resetPasswordUrl = new URL(`${base}/api/v1/authentication/password/reset/`)
+// end Authentication
+
+export const feedUrl = new URL(`${base}/api/v1/feed/`)
+
+
+// Like
+export function likeCreate(id: string | string[] | undefined): URL {
+    const url = new URL(`${base}/api/v1/shops/${id}/like/`)
+    return url
+}
+
+export function likeDestroy(id: string | string[] | undefined): URL {
+    const url = new URL(`${base}/api/v1/shops/${id}/unlike/`)
+    return url
+}
+// End like
+
+// Follow
+export function followCreate(id: string | string[] | undefined): URL {
+    const url = new URL(`${base}/api/v1/users/${id}/follow/`)
+    return url
+}
+
+export function followDestroy(id: string | string[] | undefined): URL {
+    const url = new URL(`${base}/api/v1/users/${id}/unfollow/`)
+    return url
+}
+// End follow
+
+
+export function userDetail(id: string | string[] | undefined, searchParams: object | undefined): URL {
+    const url = new URL(`${base}/api/v1/users/${id}/`)
+    if (searchParams) {
+        for (const [key, value] of Object.entries(searchParams)) {
+            url.searchParams.append(key, value)
+        }
+    }
+
+    return url
+}
+
+// Coffee shops
+export function coffeeList(searchParams: object | undefined): URL {
+    const url = new URL(`${base}/api/v1/shops/`)
+    if (searchParams) {
+        for (const [key, value] of Object.entries(searchParams)) {
+            url.searchParams.append(key, value)
+        }
+    }
+
+    return url
+}
+
+export function coffeeDetail(id: string | string[] | undefined, searchParams: object | undefined): URL {
+    const url = new URL(`${base}/api/v1/shops/${id}/`)
+    if (searchParams) {
+        for (const [key, value] of Object.entries(searchParams)) {
+            url.searchParams.append(key, value)
+        }
+    }
+
+    return url
+}
+// end coffee shops
+
+// Review
+export function reviewList(searchParams: object | undefined): URL {
+    const url = new URL(`${base}/api/v1/reviews/`)
+    if (searchParams) {
+        for (const [key, value] of Object.entries(searchParams)) {
+            url.searchParams.append(key, value)
+        }
+    }
+
+    return url
+}
+
+export function reviewCreate(): URL {
+    const url = new URL(`${base}/api/v1/reviews/`)
+    return url
+}
+// end Review
