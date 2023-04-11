eab78834913f60758429b7f7ee39219f384b0fc1
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-11 14:15:26 -0600

    add custom header to signin

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,43 +1,51 @@
-import { signIn, getCsrfToken, getProviders } from 'next-auth/react'
+import Link from 'next/link';
+import styles from '@styles/forms.module.css';
+import { getCsrfToken, getProviders, signIn } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import styles from '@styles/forms.module.css'
-import { getSession } from 'next-auth/react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
+import Head from 'next/head';
 const Signin = ({ csrfToken, providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter()
-    const { error } = router.query
+    const { error, registration } = router.query
+
 
     return (
-        <div className={styles.formContainer}>
-            <h3>Login</h3>
-            <form className={styles.form} method="post" action="/api/auth/callback/credentials">
-                <input name='csrfToken' type='hidden' defaultValue={csrfToken} />
-                <label className={styles.label} htmlFor="username">Username</label>
-                <input id="username" name="username" className={styles.input} placeholder='username' value={"eduardo"} required />
-                <label className={styles.label} htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" className={styles.input} value={"Stalker88"} placeholder='password' required />
-                <Link href={"/auth/password-reset"}>Forgot password?</Link>
-                <button type="submit" className={styles.submit}>Login</button>
-                {
-                    providers &&
-                    Object.values(providers).map(provider => {
-                        if (provider.name === 'credentials') {
-                            return
-                        }
-                        return (
-                            <div key={provider.name} style={{ marginBottom: 0 }}>
-                                <button className={styles.submit} onClick={() => signIn(provider.id)} >
-                                    Sign in with{' '} {provider.name}
-                                </button>
-                            </div>
-                        )
-                    })
-                }
-            </form>
-            {error ? <div className={styles.requestError}>The username and/or the password are not valid!</div> : null}
-        </div>
+        <>
+            <Head>
+                <title>{`Tamper | Signin`}</title>
+                <meta name="description" content={`Login to tamper to find the best speciality coffee shops in Gdl`} />
+            </Head>
+            <div className={styles.formContainer}>
+                <h3>Login</h3>
+                {registration === 'success' ? <div className={styles.requestSuccess}>Registration done! Please try to login</div> : null}
+                <form className={styles.form} method="post" action="/api/auth/callback/credentials">
+                    <input name='csrfToken' type='hidden' defaultValue={csrfToken} />
+                    <label className={styles.label} htmlFor="username">Username</label>
+                    <input id="username" name="username" className={styles.input} placeholder='username' value={"eduardo"} required />
+                    <label className={styles.label} htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" className={styles.input} value={"Stalker88"} placeholder='password' required />
+                    <Link href={"/auth/password-reset"}>Forgot password?</Link>
+                    <button type="submit" className={styles.submit}>Login</button>
+                    {
+                        providers &&
+                        Object.values(providers).map(provider => {
+                            if (provider.name === 'credentials') {
+                                return
+                            }
+                            return (
+                                <div key={provider.name} style={{ marginBottom: 0 }}>
+                                    <button className={styles.submit} onClick={() => signIn(provider.id)} >
+                                        Sign in with{' '} {provider.name}
+                                    </button>
+                                </div>
+                            )
+                        })
+                    }
+                </form>
+                {error ? <div className={styles.requestError}>The username and/or the password are not valid!</div> : null}
+            </div>
+        </>
     )
 }
 
