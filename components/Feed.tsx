44e44cd7c8984473fc4941382d31a3730ff2934d
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-12 12:42:17 -0600

    Add key to feed items

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,11 @@
 import Error from '@components/Error';
 import FeedItem from '@components/FeedItem';
 import Loader from '@components/Loader';
-import React, { useEffect } from 'react';
+import React, { useId } from 'react';
 import styles from '@styles/feed.module.css';
 import { feedUrl } from '@urls/index';
 import { fetchGet } from '@fetchUtils/useFetch';
 import { useQuery } from 'react-query';
-import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import Cross from '@components/Cross';
 
@@ -18,10 +17,12 @@ const Feed = ({ openFeed, setOpenFeed }: FeedProps) => {
         queryFn: () => fetchGet(feedUrl, token),
         enabled: status !== 'loading' && status !== 'unauthenticated'
     })
+    const feedId = useId()
 
     if (status === 'unauthenticated') {
         return null
     }
+
     return (
         <div className={`${styles.container} ${openFeed ? '' : styles.hidden}`}>
             <h3>Feed</h3>
@@ -30,7 +31,7 @@ const Feed = ({ openFeed, setOpenFeed }: FeedProps) => {
                 {error ? <Error message={"We couldn't get your feed. Please try refreshing the page."} /> : null}
                 {isLoading ? <Loader /> : null}
                 {data ? <ul>
-                    {data?.results?.map(({ action, target, user, created }: Feed) => <li>
+                    {data?.results?.map(({ action, target, user, created }: Feed, index: number) => <li key={`${feedId}-${index}`}>
                         <FeedItem action={action} target={target} user={user} created={created} />
                     </li>)
                     }
