eab1ab54d96b5bb59e311e2270e02136f8c5fa9a
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-16 00:13:13 -0600

    refactor improts on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '@components/Layout'
-import { SessionProvider } from "next-auth/react"
-import { QueryClient, QueryClientProvider } from 'react-query'
-import '@fortawesome/fontawesome-svg-core/styles.css';
-// Prevent fontawesome from adding its CSS since we did it manually above:
+import Layout from '@components/Layout';
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false;
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { SessionProvider } from 'next-auth/react';
+import '@styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import type { AppProps } from 'next/app'
+
+// Prevent fontawesome from adding its CSS since we did it manually above:
+config.autoAddCss = false
 
 const queryClient = new QueryClient()
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
