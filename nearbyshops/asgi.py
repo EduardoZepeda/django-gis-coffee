868f7514b27a14cd7d487442a0870d1d51b91de0
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-17 11:43:02 -0600

    load django asgi application before any app

--- nearbyshops/asgi.py
+++ nearbyshops/asgi.py
@@ -1,21 +1,25 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from django.conf import settings
-from chat.middleware import TokenAuthMiddleware
-import chat.routing
+from django.core.asgi import get_asgi_application
 
+# before anything make sure the application is loaded, so apps can be imported
+# please don't rearrange imports here because the order is important
 current_settings = (
     "nearbyshops.settings" if settings.DEBUG else "nearbyshops.dev_settings"
 )
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", current_settings)
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+from chat.middleware import TokenAuthMiddleware
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": TokenAuthMiddleware(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
