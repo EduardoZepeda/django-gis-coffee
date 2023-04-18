bca3dd4b06968c88fa666bd18909155beb78a484
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-04-18 13:33:12 -0600

    add preconnect tags to openstreet maps' servers

--- components/CoffeeDetailMap.tsx
+++ components/CoffeeDetailMap.tsx
@@ -2,20 +2,27 @@
 import { LatLngLiteral } from 'leaflet'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import { Marker } from 'react-leaflet'
-
+import Head from 'next/head'
 
 const CoffeeDetailMap = ({ lat, lng }: LatLngLiteral) => {
 
     return (
-        <MapContainer center={{ lat, lng }} zoom={16} scrollWheelZoom={true} style={{
-            height: "100%", width: "100%", minHeight: '400px', zIndex: "9"
-        }}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={{ lat, lng }} />
-        </MapContainer >
+        <>
+            <Head>
+                <link rel="preconnect" href="https://a.tile.openstreetmap.org" crossOrigin='' />
+                <link rel="preconnect" href="https://b.tile.openstreetmap.org" crossOrigin='' />
+                <link rel="preconnect" href="https://c.tile.openstreetmap.org" crossOrigin='' />
+            </Head>
+            <MapContainer center={{ lat, lng }} zoom={16} scrollWheelZoom={true} style={{
+                height: "100%", width: "100%", minHeight: '400px', zIndex: "9"
+            }}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={{ lat, lng }} />
+            </MapContainer >
+        </>
     )
 }
 
--- components/Map.tsx
+++ components/Map.tsx
@@ -2,20 +2,27 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import { GdlLocation } from '@components/GuadalajaraLocation';
 import { LocationMarker } from '@components/LocationMarker'
-
+import Head from 'next/head';
 
 const Map = () => {
 
     return (
-        <MapContainer center={GdlLocation} zoom={12} scrollWheelZoom={true} style={{
-            height: "100%", width: "100%", minHeight: '800px', zIndex: "9"
-        }}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <LocationMarker />
-        </MapContainer >
+        <>
+            <Head>
+                <link rel="preconnect" href="https://a.tile.openstreetmap.org" crossOrigin='' />
+                <link rel="preconnect" href="https://b.tile.openstreetmap.org" crossOrigin='' />
+                <link rel="preconnect" href="https://c.tile.openstreetmap.org" crossOrigin='' />
+            </Head>
+            <MapContainer center={GdlLocation} zoom={12} scrollWheelZoom={true} style={{
+                height: "100%", width: "100%", minHeight: '800px', zIndex: "9"
+            }}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <LocationMarker />
+            </MapContainer >
+        </>
     )
 }
 
