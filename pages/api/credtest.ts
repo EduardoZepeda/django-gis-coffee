ef0546cf587becc9f41f3805d9f4a846117a2ce5
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2025-10-14 14:28:24 -0600

    try to mimick endpoint

--- pages/api/credtest.ts
+++ pages/api/credtest.ts
@@ -8,11 +8,22 @@ const credtest: NextApiHandler<User> = async (request: NextApiRequest, response:
         response.status(405).end()
         return
     }
-    return fetch(loginUrl, {
-        method: 'POST',
-        body: JSON.stringify(request.body),
-        headers: new Headers({ 'content-type': 'application/json' })
-    })
+    try {
+        const result = await fetch(loginUrl, {
+            method: 'POST',
+            body: JSON.stringify(request.body),
+            headers: new Headers({ 'content-type': 'application/json' })
+        })
+        const data = await result.json()
+        response.json({ "data": data, "status": "ok" })
+        response.status(result.status).end
+        return result
+    }
+    catch (e) {
+        response.json({ "error": e })
+        response.status(500).end()
+        return
+    }
 
 }
 
